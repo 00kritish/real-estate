cb81d7308726d5e467459fa82b05b675d471d7e4
00kritish <kritishmhrzn123@gmail.com>
2026-06-24 22:53:27 +0545

    deployment update

---
+++ server/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.export={
+    apps:[{
+        name:"project-management",
+        script:"npm",
+        args:"run dev",
+        env:{
+            NOD_ENV:"development"
+        },
+    },
+],
+};
